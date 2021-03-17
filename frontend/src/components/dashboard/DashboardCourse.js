dc6cac232fad36482e0b1063545faaebe2ba8bb2
Baraiya Sameep <baraiya.sameep@gmail.com>
2021-03-17 19:00:55 +0530

    View Subject Done

--- frontend/src/components/dashboard/DashboardCourse.js
+++ frontend/src/components/dashboard/DashboardCourse.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Course Componets
 import FindCourse from '../models/course/FindCourse';
+import ViewCourse from '../models/course/ViewCourse';
+
+// Context
+import CourseContext from '../../context/course/courseContext';
 
 const DashboardCourse = () => {
+  const courseContext = useContext(CourseContext);
+  const { courses } = courseContext;
+
   return (
     <div>
       <FindCourse defaultSelect='all' />
+      {courses &&
+        courses.map((it, index) => <ViewCourse course={it} key={index} />)}
     </div>
   );
 };
--- frontend/src/components/models/course/ViewCourse.js
+++ frontend/src/components/models/course/ViewCourse.js
@@ -1,7 +1,111 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Card, Badge } from 'react-bootstrap';
+import { iconCourse } from '../../layout/Icon';
+import PropTypes from 'prop-types';
 
-const ViewCourse = () => {
-  return <div>View Course</div>;
+// Utils
+import unf from '../../../utils/unf';
+
+const ViewCourse = ({ course = null }) => {
+  if (!unf(course)) {
+    return null;
+  }
+
+  const {
+    courseCode,
+    courseName,
+    courseDescription,
+    courseType,
+    department,
+    noOfSem,
+    monthPerSem,
+    isOutdated,
+    isFreezed,
+    createdAt,
+    updatedAt,
+    crudInfo,
+  } = course;
+
+  if (!unf(courseCode) || !unf(courseName)) {
+    return null;
+  }
+
+  const crudInfoType = (type) => {
+    switch (type) {
+      case 'COURSE_CREATE':
+        return 'Course Created';
+      case 'COURSE_UPDATE':
+        return 'Course Updated';
+      default:
+        return 'Unexpected CRUD Operation';
+    }
+  };
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          {iconCourse} {courseName}{' '}
+          <Badge className='text-muted'>({courseCode})</Badge>
+        </Card.Title>
+        <Card.Subtitle className='mb-1'>
+          <Badge variant='secondary'>{courseType}</Badge>{' '}
+          <Badge variant='success'>{department}</Badge>{' '}
+          <Badge variant='primary'>
+            {unf(noOfSem) && `No fo Sems: ${noOfSem}`}
+          </Badge>{' '}
+          <Badge variant='info'>
+            {unf(monthPerSem) && `Month per Sem: ${monthPerSem}`}
+          </Badge>{' '}
+          <Badge variant='danger'>
+            {unf(isOutdated) && isOutdated && 'outdated'}
+          </Badge>{' '}
+          <Badge variant='danger'>
+            {unf(isFreezed) && isFreezed && 'freezed'}
+          </Badge>
+        </Card.Subtitle>
+        {unf(createdAt) && (
+          <div className='mb-1'>
+            <strong>Subject Created At:</strong>{' '}
+            {new Date(createdAt).toLocaleString('en-BZ', {
+              hour12: true,
+            })}
+          </div>
+        )}
+        {unf(updatedAt) && (
+          <div className='mb-1'>
+            <strong>Subject Updated At:</strong>{' '}
+            {new Date(updatedAt).toLocaleString('en-BZ', {
+              hour12: true,
+            })}
+          </div>
+        )}
+        {unf(courseDescription) && (
+          <Fragment>
+            <div className='mb-1'>
+              <strong>Description: </strong>
+              <br />
+              {courseDescription}
+            </div>
+          </Fragment>
+        )}
+        {unf(crudInfo) && (
+          <Fragment>
+            <div className='mb-1'>
+              <strong>Last CRUD Opertaion :</strong>
+              <br />
+              {crudInfoType(crudInfo.type)}
+              <span className='text-muted'> - By {crudInfo.by}</span>
+            </div>
+          </Fragment>
+        )}
+      </Card.Body>
+    </Card>
+  );
+};
+
+ViewCourse.propTypes = {
+  subject: PropTypes.object,
 };
 
 export default ViewCourse;
